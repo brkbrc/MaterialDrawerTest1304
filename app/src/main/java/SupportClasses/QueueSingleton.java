ad6f929da7ee42a2cc1504d9b583c01c13651057
brkbrc <burakburucu@hotmail.com>
2017-05-31 14:20:38 +0200

    new

--- app/src/main/java/SupportClasses/QueueSingleton.java
+++ app/src/main/java/SupportClasses/QueueSingleton.java
@@ -1,15 +1,19 @@
 package SupportClasses;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 
 public class QueueSingleton {
     private static QueueSingleton mInstance;
     private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
     private static Context mContext;
 
     private QueueSingleton(Context context){
@@ -17,6 +21,24 @@ public class QueueSingleton {
         mContext = context;
         // Get the request queue
         mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+
+
     }
 
     public static synchronized QueueSingleton getInstance(Context context){
@@ -41,5 +63,8 @@ public class QueueSingleton {
     public<T> void addToRequestQueue(Request<T> request){
         // Add the specified request to the request queue
         getRequestQueue().add(request);
+    }
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
     }
 }
